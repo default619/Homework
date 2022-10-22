63441e2079ddbcdae0674c9fb4a77adaaf2e896e
Andrew <andrew_bakheet@icloud.com>
2022-10-22 13:12:11 -0700

    2nd commit got a little closer to solving the ! problem

--- src/HW18.java
+++ src/HW18.java
@@ -5,12 +5,12 @@ public class HW18 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a whole number: ");
         int num1 = scan.nextInt();
-        int count = 1;
-        int result = 0;
-        while (count<=num1){
-              result=num1*count+result;count++; // 5*1=5  2*5=10 3*5=15 4*5=20
-        }
-        System.out.println("The factorial of "+num1+" is "+result);
+        int result;
+        //int fResult;
+        int count =1;
+        while(count <= num1){result=count*num1;  int fResult=result*count;
+            System.out.println("The factorial of "+num1+" is: "+fResult);count++;}
+
 
     }
 }
