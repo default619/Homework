d78635b9593dd8f56f6540bfcc8f1082007c5faa
Andrew Bakheet <andrew_bakheet@icloud.com>
2022-11-15 03:02:01 -0600

    Factorial Homework

--- src/Homework_5/HW18.java
+++ src/Homework_5/HW18.java
@@ -2,17 +2,21 @@ package Homework_5;
 
 import java.util.Scanner;
 
+import static Homework_5.FactorialMethod.Factorial;
+
 public class HW18 {
+
     public static void main(String[] args) {
+        /*
+        Write a code where user is asked to enter an integer number:
+        Using the given number find the factorial of that number and Print the result.
+        example: 5 ==> 5! = 5*4*3*2*1 = 120
+         */
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a whole number: ");
-        int num1 = scan.nextInt();
-        int result;
-        //int fResult;
-        int count =1;
-        while(count <= num1){result=count*num1;  int fResult=result*count;
-            System.out.println("The factorial of "+num1+" is: "+fResult);count++;}
-
-
+        int num = scan.nextInt();
+        int result = Factorial(num);
+        System.out.println("The factorial of "+num+" is "+result);
     }
+
 }
