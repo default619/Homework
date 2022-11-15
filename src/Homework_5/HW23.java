df2052475452d27ebfdf948641584fb7373322fb
Andrew Bakheet <andrew_bakheet@icloud.com>
2022-11-15 03:02:33 -0600

    HW23 explanation

--- src/Homework_5/HW23.java
+++ src/Homework_5/HW23.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class HW23 {
     public static void main(String[] args) {
+        /*
+        Write a program where the user is being asked to enter a String and that will count how many times
+        "a" or 'A'   is   found   in that entered String
+         */
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a sentence:");
         String str = scan.nextLine();
