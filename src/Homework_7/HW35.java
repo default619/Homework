b496fea0e58fea9fdffc6efaadc46f55c6db8d25
Andrew Bakheet <andrew_bakheet@icloud.com>
2022-11-16 01:53:29 -0600

    HW35 done!

---
+++ src/Homework_7/HW35.java
@@ -0,0 +1,21 @@
+package Homework_7;
+
+public class HW35 {
+    /*
+    Write a method where it takes 2 strings as a parameter and returns true when 2 strings are equal and
+    returns false when they are not equal.
+     */
+    public static boolean AreEqual (String str1, String str2){
+        boolean AreTheyEqual = false;
+        if(str1.equals(str2)){
+            AreTheyEqual = true;
+        }
+        return AreTheyEqual;
+    }
+
+    public static void main(String[] args) {
+        boolean AreTheyEqual = AreEqual("Hello Andy", "Hello Andy");
+        System.out.println(AreTheyEqual);
+
+    }
+}
