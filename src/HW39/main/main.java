0144c308b876d81fb59aa8e1a262d557f1a514fc
Andrew Bakheet <andrew_bakheet@icloud.com>
2022-11-18 23:48:04 -0600

    HW39

---
+++ src/HW39/main/main.java
@@ -0,0 +1,10 @@
+package HW39.main;
+
+import HW39.methods.PrintFullName;
+
+public class main {
+    public static void main(String[] args) {
+        PrintFullName.FullName();
+
+    }
+}
---
+++ src/HW39/methods/PrintFullName.java
@@ -0,0 +1,16 @@
+package HW39.methods;
+
+public class PrintFullName {
+
+    public static void FullName (){
+        String firstName = "Andrew";
+        String lastName = "Bakheet";
+        int age = SetFullNameAndReturnAge.SetFullNameAndReturnAge(firstName,lastName);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(age);
+
+
+
+    }
+}
