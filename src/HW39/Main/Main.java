883987530453a31634f669d1e9a7a58670fe0618
Andrew Bakheet <andrew_bakheet@icloud.com>
2022-11-21 21:40:58 -0600

    HW29 revised

--- src/HW39/Main/Main.java
+++ src/HW39/Main/Main.java
@@ -4,8 +4,11 @@ import HW39.Classes.Person;
 public class Main {
 
     public static void main(String[] args) {
-        Person person1 = new Person("James","Gosling");
-        person1.printAge(67);
-        person1.printSSN();
+        Person person1 = new Person();
+        person1.setFirstName("James");
+        person1.setAge(67);
+        System.out.println(person1.getFirstName());
+
+        
     }
 }
