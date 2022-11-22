f058714e254a6ca4c44013810e539c11588c7e23
Andrew Bakheet <andrew_bakheet@icloud.com>
2022-11-22 00:28:16 -0600

    updated

--- src/Methods/ScanMultipleNumbersMethod.java
+++ src/Methods/ScanMultipleNumbersMethod.java
@@ -12,10 +12,26 @@ public class ScanMultipleNumbersMethod {
         System.out.println("How many numbers would you like to enter?");
         int count = scan.nextInt();
         for (int i = 0; i < count; i++) {
-            System.out.println("Enter the (" + (i + 1) + ") number:");
-            int num = scan.nextInt();
-            nums.add(num);
+            if (i==0){
+                System.out.println("Enter the "+(i+1)+"st number:");
+                int num = scan.nextInt();
+                nums.add(num);
+            } else if (i==1) {
+                System.out.println("Enter the "+(i+1)+"nd number:");
+                int num = scan.nextInt();
+                nums.add(num);
+            } else if (i==2) {
+                System.out.println("Enter the "+(i+1)+"rd number:");
+                int num = scan.nextInt();
+                nums.add(num);
+            }else {
+                System.out.println("Enter the "+(i+1)+"th number:");
+                int num = scan.nextInt();
+                nums.add(num);
+            }
         }
         return nums;
     }
 }
+
+
