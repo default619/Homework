b79900fcefa88c402aa2dd883e3ccf1ffc824419
Andrew Bakheet <andrew_bakheet@icloud.com>
2022-11-21 21:53:54 -0600

    HW31

--- src/Homework_6/HW29.java
+++ src/Homework_6/HW29.java
@@ -1,5 +1,6 @@
 package Homework_6;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class HW29 {
@@ -9,27 +10,13 @@ public class HW29 {
         //- Print the values that can be divided by 2 or 3
 
         int[] num = {90, 34, 23, 87, 45, 36, 98};
-        int[] result = new int[num.length]; // not sure how to create an array with a dynamic length (dependent on the amount of numbers needed)
-        int j = 0;
+        ArrayList<Integer> result = new ArrayList<>();
         for (int i = 0; i < num.length; i++) {
             if (num[i] % 2 == 0 || num[i] % 3 == 0) {
-                result[j] = num[i]; j++;
+                result.add(num[i]);
             }
         }
-        System.out.println("The values that can be divided by 2 and 3 are: " + Arrays.toString(result));
-        System.out.println();
-        System.out.println("*************************************************************************");
-        System.out.println();
-        System.out.print("The values that can be divided by 2 and 3 are: "); //another way of getting the same answer.
-        int[] num2 = {90, 34, 23, 87, 45, 36, 98};
-        for (int i = 0; i < num2.length; i++) {
-            if (num2[i] %2 == 0 || num[i] % 3 == 0) {
-                System.out.print("("+num[i]+") " );
-            }
-
-        }
-
-
+        System.out.println("The values dividable by 2 or 3 are: "+result);
     }
 }
 
